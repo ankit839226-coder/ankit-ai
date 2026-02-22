0e9b15542b46c6eb965e9e1bd907206357c95071
ankit839226-coder <ankit839226@gmail.com>
2026-02-22 09:35:20 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from openai import OpenAI
 import os
 
-app = Flask(_name_)
+app = Flask(__name__)
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 @app.route("/")
