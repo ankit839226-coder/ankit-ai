36743c2fec11aa404f104dbd0ff510cb5c9bcbf4
ankit839226-coder <ankit839226@gmail.com>
2026-02-22 09:49:39 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,9 +1,8 @@
-from flask import Flask, request, jsonify
+rom flask import Flask, request, jsonify
 from openai import OpenAI
 import os
 
-app = Flask(__name__)
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+app = Flask(_name_)
 
 @app.route("/")
 def home():
@@ -11,12 +10,18 @@ def home():
 
 @app.route("/chat", methods=["POST"])
 def chat():
-    user_message = request.json["message"]
+    api_key = os.environ.get("OPEN_API_KEY")
+    
+    if not api_key:
+        return jsonify({"error": "API key not found"}), 500
+
+    client = OpenAI(api_key=api_key)
+
+    user_message = request.json.get("message")
 
     response = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[
-            {"role": "system", "content": "You are a helpful assistant"},
             {"role": "user", "content": user_message}
         ]
     )
